5b3ad2d536cf3560c37b9a7f9115ce9987627e68
hamzatalha77 <hamza_ta7@hotmail.com>
2024-04-11 10:29:12 +0000

    commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    tailwindcss: {
+      content: ['./src/**/*.{js,jsx,ts,tsx}']
+    },
+    autoprefixer: {}
+  }
 }
